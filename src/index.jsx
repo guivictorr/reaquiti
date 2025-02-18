310fd92892ee7048b2ee409597481c39a6845839
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-02-18 20:03:40 -0300

    feat: add useState hook

--- src/index.jsx
+++ src/index.jsx
@@ -161,9 +161,46 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null;
+let hookIndex = null;
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
+}
+
+function useState(initial) {
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action(hook.state);
+  });
+
+  const setState = (action) => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  };
+
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
 }
 
 function updateHostComponent(fiber) {
@@ -231,12 +268,19 @@ function reconcileChildren(wipFiber, elements) {
 const Reaquiti = {
   createElement,
   render,
+  useState,
 };
 
 /** @jsx Reaquiti.createElement */
-function App(props) {
-  return <h1>Hi, {props.name}</h1>;
+function Counter() {
+  const [count, setCount] = Reaquiti.useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Add</button>
+    </div>
+  );
 }
-const element = <App name="Fulano" />;
+const element = <Counter />;
 const container = document.getElementById("root");
 Reaquiti.render(element, container);
